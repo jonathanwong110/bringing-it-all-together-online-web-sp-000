da24dc8ff6bfd4de9c516c14b90670d6fe749dff
Jonathan Wong <jonathan.wong110@gmail.com>
2019-06-03 17:01:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,10 +65,12 @@ class Dog
   end
   
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
-    self.new(id, name, grade)
+    hash_attributes = {
+      :id => row[0],
+      :name => row[1],
+      :breed => row[2]
+    }
+    self.new(hash_attributes)
   end
   
   def self.find_by_name(name)
